51991a1d64a44406c50de470e016de791d89e587
Duncan Sparks <duncan.sprks@gmail.com>
2020-06-28 13:20:46 -0700

    Add scene transitions

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+	[SerializeField]
+	private Object targetScene;
+
+	void OnTriggerEnter()
+	{
+		SceneManager.LoadScene(targetScene.name);
+	}
+}
